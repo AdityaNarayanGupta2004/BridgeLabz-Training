9ba7750efe6b98399e6e2770181b44d499ed8630
AdityaNarayanGupta2004 <adityanarayangupta812@gmail.com>
2026-01-12 22:05:54 +0530

    [Aditya Narayan Gupta] Add . Added the solution of the regex related problems

---
+++ collections-practice/gcr-codebase/regex-and-junit/regex/LicensePlateValidator.java
@@ -0,0 +1,13 @@
+public class LicensePlateValidator {
+    public static void main(String[] args) {
+        String[] plates = {"AB1234","A12345","AB123"};
+        String regex = "^[A-Z]{2}[0-9]{4}$";
+        for(String plate : plates){
+            if(plate.matches(regex)){
+                System.out.println(plate + "->Valid");
+            }else{
+                System.out.println(plate + "->Invalid");
+            }
+        }
+    }
+}
